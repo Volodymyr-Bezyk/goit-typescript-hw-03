5ba12547408a1c9fe73157be0c21058474a74cec
Volodymyr-Bezyk <99117313+Volodymyr-Bezyk@users.noreply.github.com>
2023-08-27 09:52:46 +0300

    ts hw 03

--- src/2.ts
+++ src/2.ts
@@ -16,9 +16,9 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+  public name: string;
+  private department: string;
+  protected salary: number;
 
   constructor(name: string, department: string, salary: number) {
     this.name = name;
@@ -33,7 +33,18 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary);
+    this.increaseSalary();
+  }
+
+  private increaseSalary() {
+    this.salary += 10000;
+    console.log("salary", this.salary);
+  }
 }
 
+const worker = new Manager("Anton", "sales", 5000);
+console.log("worker", worker);
 
 export {};
--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,69 @@
-const key = new Key();
+interface IKey {
+  getSignature: () => number;
+}
+interface IPerson {
+  getKey: () => Key;
+}
 
+class Key implements IKey {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    console.log("keySiganture", this.signature);
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey() {
+    console.log("PersonKey", this.key);
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: boolean;
+  public key: Key;
+  public tenants: Person[];
+
+  comeIn(person: Person): void {
+    if (this.door === true) {
+      this.tenants.push(person);
+      console.log("tenants", this.tenants);
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super();
+    this.key = key;
+    this.tenants = [];
+  }
+
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Door opened");
+    } else {
+      this.door = false;
+      console.log("Door closed");
+    }
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
-
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
